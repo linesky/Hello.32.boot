c4f67bdf7045173f4a4b01ec452d28f1f6af7a12
sky line <32512284+linesky@users.noreply.github.com>
2024-04-27 23:07:29 +0100

    Add files via upload

--- make.sh
+++ make.sh
@@ -1,6 +1,8 @@
 printf "\x1bc\x1b[43;37m "
 nasm -o /tmp/boot.bin boot.asm
-nasm -o /tmp/hello.com program.asm
-dd if=/tmp/hello.com of=/tmp/boot.bin seek=1 conv=notrunc
+gcc -c kernel.c -o /tmp/kernel.o -nostdlib
+ld -T link.ld /tmp/kernel.o -o /tmp/hello.com -nostdlib
+objcopy -O binary  /tmp/hello.com  /tmp/hellos.com
+dd if=/tmp/hellos.com of=/tmp/boot.bin seek=1 conv=notrunc
 cp -f /tmp/boot.bin ./
 /usr/bin/qemu-system-x86_64 -boot a -fda boot.bin
